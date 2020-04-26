4a6e3d7456c686595dce4abcda55d68965ea4a08
amezousan <goesbyamezou@gmail.com>
2020-04-26 11:37:42 +0200

    Release 20200426 Fix (#17)
    * Fix
    * Blog 20200426

---
+++ aws_s3_sync_with_git_status.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+while getopts b: option
+do
+case "${option}"
+in
+    b) S3_BUCKET=${OPTARG};;
+esac
+done
+
+echo "Target: ${S3_BUCKET}"
+
+if [ -z ${S3_BUCKET} ]
+then
+    echo "How to use: -s3 <S3 Bucket Name>"
+    exit 1
+fi
+
+FILES=()
+for i in $( git status -s | sed 's/\s*[a-zA-Z"?]\+ \(.*\)/\1/' | sed 's/"//g' | grep "public/"); do
+    FILES+=( "$i" )
+done
+# echo "${FILES[@]}"
+
+CMDS=()
+for i in "${FILES[@]}"; do
+    CMDS+=("--include=$i""*")
+done
+# echo ${CMDS[@]}
+
+echo "${CMDS[@]}" | xargs aws s3 sync ./public s3://${S3_BUCKET}/public --delete --exclude "*"
